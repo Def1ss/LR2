71cca67193affbaf5630453643a09c97393b7578
Defis <borsuk.daniil7@gmail.com>
2024-09-26 10:28:46 +0300

    Task_6.6

--- Task_6.cpp
+++ Task_6.cpp
@@ -6,10 +6,10 @@ int main()
 {
     double A,  D ,x , y , z ,itog , p , K , C , n , m ;
     std::cout << "Введите 2 значения "<< std::endl;
-    std::cin >> n >> x >> K >> D >> m ;
-    A = abs(n + m);
-    D = tan(x);
-    itog = 1,29 + (K/A) + D * D ;
+    std::cin >> y >> x >> K >> D >> C ;
+    A = x + y;
+    D = abs(C - A);
+    itog = 10.1 + (A/C) + (D/K/K);
     std::cout << itog ;
 
 }
