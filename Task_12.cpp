57156ac8679f67637f93122f4565c358d507f0c7
Defis <borsuk.daniil7@gmail.com>
2024-11-02 14:01:55 +0300

    Task_12.2

--- Task_12.cpp
+++ Task_12.cpp
@@ -1,59 +1,57 @@
-#include <iostream>
+double a, b, c;
 
-
-
-
-
-double com(double v)
-{
-    if (v < 0) return -1; // Отрицательные числа не имеют действительного корня
-
-    double L = 0;
-    double R = v;
-    double M;
-
-    while (R - L > 0.0000000001) // заданная точность
-    {
-        M = (L + R) / 2; // пересчет M
-        if (M * M <= v)
-        {
-            L = M; // сужаем диапазон к правой части
-        }
-        else 
-        {
-            R = M; // сужаем диапазон к левой части
-        }
+double getsqrt(double x) {
+    double l = 0, r = 1e9;
+    for (int i = 0; i < 1000; i++) {
+        double mid = (l + r) / 2;
+        if (mid * mid > x) r = mid; else l = mid;
     }
-    
-    return M; // возвращаем найденный корень
+
+    return l;
 }
 
-int main ()
-{
-    double a, b , c , D,x1 , x2 , x3 , x4 ;
-    std::cout << "Введите значения a , b , c для уравнения вида : ax^4 + bx^2 + c = 0 " << std::endl;
+int main() {
+    std::cout << "Введите a, b, c\n";
     std::cin >> a >> b >> c;
-    D = (b * b )- (4 * a * c);
-    if (D > 0)
-    {
-        x1 = com((-b - com(D)) / 2 * a);
-        x2 =  -x1;
-        x3 = com((-b + com(D)) / 2 * a);
-        x4 = -x3;
-        std::cout << x1 << " " << x2 << " "<< x3 << " " << x4;
+
+    double D = b * b - 4 * a * (c - 2 * a);
+    if (D < 0) {
+        std::cout << "Нет корней";
+        return 0;
     }
-    else 
-    {
-        if (D = 0)
-        {
-            x1 = com(-b/(2*a));
-            x2 = -x1;
-            std::cout << x1 << " "<< x2;
+
+    double d1 = ((-b - getsqrt(D)) / (2 * a)), d2 = ((-b + getsqrt(D)) / (2 * a));
+
+    std::cout << "Корни :\n";
+
+    if (D == 0) {
+        double D2 = (d1 * d1) - 4;
+        if (D2 >= 0) {
+            if (D2 == 0) std::cout << d1 / 2 << '\n';
+            else {
+                std::cout << (d1 - getsqrt(D2)) / 2 << '\n';
+                std::cout << (d1 + getsqrt(D2)) / 2 << '\n';
+            }
         }
-        else
-        {
-            std::cout << "Дискриминант отрицательный" << std::endl;
+    } else {
+        double D2 = (d1 * d1) - 4;
+        if (D2 >= 0) {
+            if (D2 == 0) std::cout << d1 / 2 << '\n';
+            else {
+                std::cout << (d1 - getsqrt(D2)) / 2 << '\n';
+                std::cout << (d1 + getsqrt(D2)) / 2 << '\n';
+            }
+        }
+
+        D2 = (d2 * d2) - 4;
+        if (D2 >= 0) {
+            if (D2 == 0) std::cout << d2 / 2 << '\n';
+            else {
+                std::cout << (d2 - getsqrt(D2)) / 2 << '\n';
+                std::cout << (d2 + getsqrt(D2)) / 2 << '\n';
+            }
         }
     }
 
+    return 0;
 }
